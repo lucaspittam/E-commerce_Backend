1b15601e4ed6a858c4e2f1eb17ee461d3c337111
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-05-19 16:35:29 -0400

    setup model objects

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    d: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      //validate is decimal
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //default value of 10
+      defaultValue: 10,
+      //validate is numeric
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
